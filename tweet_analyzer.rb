8d68e62c9fb20be8d5ac08858d41585366ff7f9c
Salman Ansari <salman.ansari@gmail.com>
2014-05-28 12:10:53 +0200

    updated to use Methods

--- tweet_analyzer.rb
+++ tweet_analyzer.rb
@@ -1,37 +1,61 @@
-puts "Hello! Welcome to Tweet Analyzer!"
 
-puts "Please enter 3 tweets:"
+# Greet user
+def greet_user
+	puts "Hello! Welcome to Tweet Analyzer!"
+	puts "Please enter 3 tweets:"
+end
+
+# Collect tweets
+def get_tweets(tweets)
+	3.times do
+		tweet = gets.strip
+		tweets << tweet
+	end
+end
+
+# Generate words
+
+def calculate_counts(counts, tweets)
+	words = tweets.map { |tweet| tweet.split }.flatten
+
+	# Calculate counts	
+	words.each do |word|
+		if !counts[word]
+			counts[word] = 1
+		else
+			counts[word] += 1
+		end
+	end
+end
+
+# Get highest word
+def get_highest_word(counts)	
+	highest_word = ""
+	max = 0
+
+	counts.each do |word, count|
+		if (count > max)
+			max = count
+			highest_word = word
+		end
+	end
+
+	highest_word
+end
+
+# Print results
+def show_results(word)
+	puts "THE MOST POPULAR WORD IS: #{word}"
+end
+
+# main.rb
+greet_user
 
 tweets = []
-3.times do
-	tweet = gets.strip
-
-	tweets << tweet
-end
-
-words = tweets.map { |tweet| tweet.split }.flatten
+get_tweets(tweets)
 
 counts = {}
-words.each do |word|
-	if !counts[word]
-		counts[word] = 1
-	else
-		counts[word] += 1
-	end
-end
+calculate_counts(counts, tweets)
 
-highest_word = ""
-max = 0
+show_results(get_highest_word(counts))
 
-counts.each do |word, count|
-	if (count > max)
-		max = count
-		highest_word = word
-	end
-end
-
-puts "THE MOST POPULAR WORD IS: #{highest_word}, WITH A COUNT OF: #{max}"
-
-puts "COUNTS HASH IS: #{counts}"
-
-puts "TWEETS ARRAY IS: #{tweets}"
